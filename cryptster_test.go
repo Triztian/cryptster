b63129ed9857730745e2df72a19a748b8227c90d
Tristian Azuara <tristian.azuara@gmail.com>
2014-11-29 18:07:11 -0800

    Finally complete sha1, bit rotation was the culprit

--- cryptster_test.go
+++ cryptster_test.go
@@ -65,13 +65,29 @@ func TestGetInts(t *testing.T) {
 	}
 }
 
+// Just to test the hex encoding
+func TestEncode(t *testing.T) {
+	var (
+		b []byte = []byte{255, 255, 0, 0}
+		s string = "ffff0000"
+	)
+	enc := hex.EncodeToString(b)
+	if s != enc {
+		t.Error("Encoding is different", s, enc)
+	}
+}
+
 // This test case verifies that the bit rotation is done properly
 func TestLrot(t *testing.T) {
 	var (
 		x64 uint64 = 0x8000000080000000
 		r64 uint64 = 0x0000000100000001
+
 		x32 uint32 = 0x80008000
 		r32 uint32 = 0x00010001
+
+		y32 uint32 = 0xF0F0F0F0
+		s32 uint32 = 0x87878787
 	)
 
 	a64 := digest.Lrot64(x64, 1)
@@ -83,25 +99,35 @@ func TestLrot(t *testing.T) {
 	if a32 != r32 {
 		t.Errorf("32-bit Left Rotation incorrect got %x expected %x", a32, r32)
 	}
+
+	b32 := digest.Lrot32(y32, 3)
+	if b32 != s32 {
+		t.Errorf("32-bit Left 3 Rotation incorrect got %x expected %x", b32, s32)
+	}
 }
 
 func TestSHA(t *testing.T) {
-	message := "The quick brown fox jumps over the lazy dog"
-	hexString := "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12"
+	var messages = map[string]string{
+		"A": "6dcd4ce23d88e2ee9568ba546c007c63d9131c1b",
+		"The quick brown fox jumps over the lazy dog": "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
+	}
+
 	dmsg := make([]byte, bytes.MinRead)
 
 	sha := digest.SHA1{}
-	reader := strings.NewReader(message)
+	for message, dig := range messages {
+		reader := strings.NewReader(message)
 
-	read, err := reader.Read(dmsg)
-	if read > 0 && err == nil {
-		computedHex := hex.EncodeToString(sha.Digest(dmsg[:read]))
-		if computedHex != hexString {
-			t.Errorf("Incorrect digest, should be \"%s\" but was \"%s\"", hexString, computedHex)
+		read, err := reader.Read(dmsg)
+		if read > 0 && err == nil {
+			computedHex := hex.EncodeToString(sha.Digest(dmsg[:read]))
+			if computedHex != dig {
+				t.Errorf("Incorrect digest SHA1(\"%s\") should be \"%s\" but was \"%s\"", message, dig, computedHex)
+			}
+
+			read, err = reader.Read(dmsg)
+		} else {
+			t.Error("Could not read data", err)
 		}
-
-		read, err = reader.Read(dmsg)
-	} else {
-		t.Error("Could not read data", err)
 	}
 }
